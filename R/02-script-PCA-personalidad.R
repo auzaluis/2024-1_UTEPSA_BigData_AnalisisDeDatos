3a1f17e7f84a6595dff6859fdfc485b0f4d9f43d
Luis Auza <luis.auza@gmail.com>
2024-06-05 00:49:28 +0000

    WIP

--- R/02-script-PCA-personalidad.R
+++ R/02-script-PCA-personalidad.R
@@ -58,8 +58,105 @@ data.frame(
 ) %>% View()
 
 
+## Dimensión 02: Aventura (*-1) ----
+
+### Definiendo el vector con las frases
+aventura <- frases2[c(7,19)]
+
+### Creando la dimensión
+PCA.aventura <- FactoMineR::PCA(
+  X = df6 %>% select(all_of(aventura)),
+  ncp = 1
+)
+
+### Eigenvalues y varianza explicada
+PCA.aventura$eig
+
+### Correlación entre la CP1 y las variables originales
+PCA.aventura$var$cor
+
+### Comparar los valores originales con la CP1
+data.frame(
+  CP1 = PCA.aventura$ind$coord,
+  df6 %>% select(all_of(aventura))
+) %>% View()
 
 
+
+## Dimensión 03: Tradicional (*-1) ----
+
+### Definiendo el vector con las frases
+tradicional <- frases2[c(9,12,14)]
+
+### Creando la dimensión
+PCA.tradicional <- FactoMineR::PCA(
+  X = df6 %>% select(all_of(tradicional)),
+  ncp = 1
+)
+
+### Eigenvalues y varianza explicada
+PCA.tradicional$eig
+
+### Correlación entre la CP1 y las variables originales
+PCA.tradicional$var$cor
+
+
+
+## Dimensión 04: Empatía ----
+
+### Definiendo el vector con las frases
+empatia <- frases2[c(1,18,3)]
+
+### Creando la dimensión
+PCA.empatia <- FactoMineR::PCA(
+  X = df6 %>% select(all_of(empatia)),
+  ncp = 1
+)
+
+### Eigenvalues y varianza explicada
+PCA.empatia$eig
+
+### Correlación entre la CP1 y las variables originales
+PCA.empatia$var$cor
+
+
+
+## Dimensión 05: introversion (*-1) ----
+
+### Definiendo el vector con las frases
+introversion <- frases2[c(21,22)]
+
+### Creando la dimensión
+PCA.introversion <- FactoMineR::PCA(
+  X = df6 %>% select(all_of(introversion)),
+  ncp = 1
+)
+
+### Eigenvalues y varianza explicada
+PCA.introversion$eig
+
+### Correlación entre la CP1 y las variables originales
+PCA.introversion$var$cor
+
+
+# Adjuntando las dimensiones al df ----
+df10 <- df6 %>% 
+  mutate(
+    extroversion = PCA.extroversion$ind$coord,
+    aventura = PCA.aventura$ind$coord *-1,
+    tradicional = PCA.tradicional$ind$coord *-1,
+    empatia = PCA.empatia$ind$coord,
+    introversion = PCA.introversion$ind$coord *-1
+  )
+
+# Crear un vector para las dimensiones
+dimensiones <- c(
+  "extroversion",
+  "aventura",
+  "tradicional",
+  "empatia",
+  "introversion"
+)
 
 
 
